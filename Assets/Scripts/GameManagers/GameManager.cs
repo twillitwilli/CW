f685e8be49c9d538d673508cd4fdab1dec79950b
Twilli _ <eclipsek24@gmail.com>
2023-11-23 16:33:28 -0800

    Can now enter all houses & businesses

--- Assets/Scripts/GameManagers/GameManager.cs
+++ Assets/Scripts/GameManagers/GameManager.cs
@@ -5,5 +5,11 @@ using QTArts.AbstractClasses;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    public bool returningPlayer { get; set; }
 
+    private void Start()
+    {
+        if (!returningPlayer)
+            Player.Instance.transform.position = new Vector3(2.83f, -0.37f, 0);
+    }
 }
